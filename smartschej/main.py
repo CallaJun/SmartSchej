39754c75eb51025c87fef5f5e552fd797061ba1c
Calla Carter <maracondo@aol.com>
2014-10-24 23:10:00 -0700

    Some gcalendar implementation with errors

--- smartschej/main.py
+++ smartschej/main.py
@@ -6,6 +6,12 @@ import webapp2
 from apiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
 import httplib2
+#from gdata.apps import service
+
+flow = OAuth2WebServerFlow(client_id='195549864721-i2juie0n9vvq5g7itrk49a4452easbse.apps.googleusercontent.com',
+                           client_secret='12haQegMUPOGvJIa6RDVfhK8',
+                           scope='https://www.googleapis.com/auth/calendar',
+                           redirect_uri='http://example.com/auth_return')
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -31,6 +37,7 @@ class NextHandler(webapp2.RequestHandler):
         created_event = service.events().quickAdd(
             calendarId='primary',
             text=eventname).execute()
+
         print created_event['id']
 
         template = jinja_environment.get_template('views/next.html')
